2eb02a3510ec6aaf9b4af2b9a01f499a183cadb2
JhonnGutierrez <jhonngutierrez.contact@gmail.com>
2023-06-06 17:44:38 -0500

    chages in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,19 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import CreatorTag from '@/components/CreatorTag'
-import favicon from '@/assets/Timezip_logo.png'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'TimeZip | Show the world when find you.',
   description: 'Show the world when find you. Select an hour and one or more countries to get his hours. Twitter transform timezone',
-  icon: favicon,
-  'google-site-verification': 'vU9hWaXYV6uhFnehqOvQ3qKfCqmFZoequTm0dIn226U'
+  icons: {
+    icon: '@/assets/Timezip_logo.png'
+  },
+  other: {
+    'google-site-verification': 'vU9hWaXYV6uhFnehqOvQ3qKfCqmFZoequTm0dIn226U'
+  }
 }
 
 export default function RootLayout ({
