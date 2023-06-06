5eb213850a4f8e0a2856d472368f1b79f02769c2
JhonnGutierrez <jhonngutierrez.contact@gmail.com>
2023-06-05 22:06:08 -0500

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import CreatorTag from '@/components/CreatorTag'
+import favicon from '@/assets/Timezip_logo.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'TimeZip | Show the world when find you.',
-  description: 'Show the world when find you. Select an hour and one or more countries to get his hours. Twitter transform timezone'
+  description: 'Show the world when find you. Select an hour and one or more countries to get his hours. Twitter transform timezone',
+  icon: favicon
 }
 
 export default function RootLayout ({
