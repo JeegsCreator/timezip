e32b71ff72c2dec8ea6041273ae3569ade154d05
Jhonn Gutierrez <jhonngutierrez.contact@gmail.com>
2023-06-30 11:03:04 -0500

    clean supabase files

--- src/supabaseClient.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { env } from './env'
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY)
