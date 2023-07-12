d3cefc08c2660a3dd5be899a4a2ea8148da8c9d8
Jhonn Gutierrez <jhonngutierrez.contact@gmail.com>
2023-07-12 09:45:11 -0500

    clean comments

--- src/app/api/timezoneList/route.ts
+++ src/app/api/timezoneList/route.ts
@@ -1,23 +1,6 @@
 import { NextResponse } from 'next/server'
-// import { supabase } from '@/supabaseClient'
 import timezones from './timezones.json'
 
 export async function GET () {
-  // const { data, error } = await supabase
-  //   .from('Country')
-  //   .select('*, Timezone(*)')
-  // const res = await fs.readFile(resolve('./timezones.json'))
-  // const data = res.toJSON()
-
-  // console.log(timezones)
-
-  // const info = readFile('./timezones.json')
-
-  // if (error !== null) {
-  //   console.log('jeegs')
-  //   console.log(error)
-  //   throw new Error('Error getting data')
-  // }
   return NextResponse.json(timezones)
-  // return NextResponse.json(data)
 }
